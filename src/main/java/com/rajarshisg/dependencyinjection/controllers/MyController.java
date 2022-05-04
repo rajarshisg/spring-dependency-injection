ee09509663c071cf7bba335f46411e7b8997ec3b
Rajarshi Sengupta <rajarshisengupta@Rajarshis-MacBook-Air.local>
2022-05-05 04:02:57 +0530

    @Primary bean

--- src/main/java/com/rajarshisg/dependencyinjection/controllers/MyController.java
+++ src/main/java/com/rajarshisg/dependencyinjection/controllers/MyController.java
@@ -1,12 +1,16 @@
 package com.rajarshisg.dependencyinjection.controllers;
 
+import com.rajarshisg.dependencyinjection.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
 
+    MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
     public String sayHello() {
-        System.out.println("Hello, World!");
-        return "Hi, folks!";
+        return greetingService.sayGreeting();
     }
 }

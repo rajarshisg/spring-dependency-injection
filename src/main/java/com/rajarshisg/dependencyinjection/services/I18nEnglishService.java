29072e019f504a1855d1e8851f07067dcfec191f
Rajarshi Sengupta <rajarshisengupta@Rajarshis-MacBook-Air.local>
2022-05-09 00:58:33 +0530

    Default @Profile

--- src/main/java/com/rajarshisg/dependencyinjection/services/I18nEnglishService.java
+++ src/main/java/com/rajarshisg/dependencyinjection/services/I18nEnglishService.java
@@ -3,7 +3,7 @@ package com.rajarshisg.dependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class I18nEnglishService implements GreetingService {
     public String sayGreeting() {
